4b3d278ea17f419df59bb9ee5e870eb781365828
rjrowell <rrowellwork@gmail.com>
2026-02-03 20:07:28 +0000

    Add junit and surefire for testing

---
+++ functional/src/test/java/com/rjtourism/BasicTest.java
@@ -0,0 +1,13 @@
+package com.rjtourism;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BasicTest {
+    
+    @Test
+    public void aTest(){
+        assertEquals(1, 1);
+    }
+}
