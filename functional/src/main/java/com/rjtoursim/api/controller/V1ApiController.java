1ecc9dfcd2c119fb48375336fda6b9eb808ff6ee
rjrowell <rrowellwork@gmail.com>
2026-02-08 17:35:40 +0000

    implementation for creating a user

--- functional/src/main/java/com/rjtoursim/api/controller/V1ApiController.java
+++ functional/src/main/java/com/rjtoursim/api/controller/V1ApiController.java
@@ -1,6 +1,13 @@
 package com.rjtoursim.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rjtoursim.api.model.CreateUserRequest;
+import com.rjtoursim.entity.User;
+import com.rjtoursim.repository.UserRepository;
 
 /**
  * Controller class that adds the implementation for genrerated
@@ -9,4 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class V1ApiController implements V1Api {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public ResponseEntity<Void> createUser(CreateUserRequest createUserRequest) {
+        
+        //Check if the username is already taken, if so return a 409 Conflict status code
+        if (userRepository.findByUsername(createUserRequest.getUsername()).isPresent()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        User user = new User(createUserRequest.getUsername(), createUserRequest.getHashedPassword());
+        userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
