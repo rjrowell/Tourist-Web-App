b944d031a01771b0e6c969350094b3cc617b6890
Reuben Rowell <Reuben.Rowell@ibm.com>
2026-02-25 23:44:02 +0000

    initialise a react project for presentation layer

--- functional/src/test/java/com/rjtourism/CalculateAcheivmentTest.java
+++ functional/src/test/java/com/rjtourism/CalculateAcheivmentTest.java
@@ -99,6 +99,7 @@ public class CalculateAcheivmentTest {
         CalculateAchievementResponse.class
     );
 
-    assertTrue(response.get5likesAchievement(), "5 Likes achievement should be awarded when user has 5 or more likes");
+    assertTrue(response.get5likesAchievement(), 
+        "5 Likes achievement should be awarded when user has 5 or more likes");
   }
 }
